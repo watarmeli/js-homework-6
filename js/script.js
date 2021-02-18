6e6471ba84a1398635d645fd32cd5fe54afe8c9b
watarmeli <watar@ukr.net>
2021-02-18 19:17:07 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const books = [
+    {
+      title: 'Последнее королевство',
+      author: 'Бернард Корнуэлл',
+      genres: ['приключения', 'историческое']
+    },
+    {
+      title: 'На берегу спокойных вод',
+      author: 'Роберт Шекли',
+      genres: ['фантастика', 'мистика']
+    },
+    {
+      title: 'Красна как кровь',
+      author: 'Ли Танит',
+      genres: ['ужасы', 'мистика', 'приключения']
+    }
+  ];
+  // Пиши код ниже этой строки
+const allGenres = books.flatMap(book => book.genres);
+const uniqueGenres = allGenres.filter((genre, index, array) => array.indexOf(genre) === index);
