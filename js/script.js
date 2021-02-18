91b3628a8c0bc609f0ba63a6f1b9be8c133078a0
watarmeli <watar@ukr.net>
2021-02-18 21:39:18 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const books = [
+  { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+  { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+  { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+  { title: 'Красна как кровь', author: 'Ли Танит', rating: 8.14 },
+  { title: 'Сны В Ведьмином Доме', author: 'Говард Лавкрафт', rating: 8.67 }
+];
+const MIN_BOOK_RATING = 8;
+// Пиши код ниже этой строки
+
+const names = books.filter(book => book.rating > MIN_BOOK_RATING).sort((a, b) => a.author.localeCompare(b.author)).flatMap(book => book.author);
